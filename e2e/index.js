2742d5bad3f18751af15eab3f28de5d3b2171beb
ext-vradulescu <ext-vradulescu@bitdefender.com>
2023-07-24 13:17:28 +0300

    DEX-14247-Ghost-inspector-integration

---
+++ e2e/index.js
@@ -0,0 +1,14 @@
+const GhostInspector = require('ghost-inspector')('d18bdb6707ceb3c84778aa8c91f9cc6ba20b7ecb');
+
+(async () => {
+  // execute a test
+  try {
+    const [results, passing, screenshotPassing] = await GhostInspector.executeTest(
+      '64be47dcca40d9f691783152',
+    );
+    console.log('Passing: ', passing);
+  } catch (err) {
+    console.error(err);
+  }
+
+})();
