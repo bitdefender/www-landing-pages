2cec6389dde30554381a4fc32f5b4710034cbe29
ext-vradulescu <ext-vradulescu@bitdefender.com>
2023-07-31 01:08:01 +0300

    DEX-14247-Ghost-inspector-integration data-testid yaml final

--- e2e/index.js
+++ e2e/index.js
@@ -1,6 +1,34 @@
 require('dotenv').config();
 const GhostInspector = require('ghost-inspector')(process.env.GI_KEY);
+
 const testId = '64c608336f03cb8cdb7d955b';
+const landingPagesToTest = [
+  'business/de/mr-summer-ready-july-2023',
+  'business/de/winback-summer-ready-july-2023',
+  'business/en/cross-sell-2023-mobile-launch',
+  'business/en/cross-sell-flash-sale-pm-2023',
+  'business/en/independenceday',
+  'business/en/mr-independence-day-2023',
+  'business/en/mr-summer-ready-aug-2023',
+  'business/en/mr-summer-ready-july-2023',
+  'business/en/ransomwaretrial',
+  'business/en/upgrade-2023-summer-ready-aug',
+  'business/en/winback-2023-independence-day',
+  'business/en/winback-summer-ready-aug-2023',
+  'business/en/winback-summer-ready-july-2023',
+  'business/es/mr-summer-ready-july-2023',
+  'business/es/winback-summer-ready-july-2023',
+  'business/fr/mr-summer-ready-july-2023',
+  'business/fr/winback-summer-ready-july-2023',
+  'business/it/mr-summer-ready-july-2023',
+  'business/it/winback-summer-ready-july-2023',
+  'business/nl/ransomwaretrial',
+  'business/ro/mr-summer-ready-july-2023',
+  'business/ro/winback-summer-ready-july-2023',
+  'consumer/en/new/cl-offer-opt',
+  'consumer/en/new/last-offer',
+  'consumer/en/new/ultimate-flv1',
+];
 
 (async () => {
   function logError(message) {
@@ -10,10 +38,10 @@ const testId = '64c608336f03cb8cdb7d955b';
     console.log('\x1b[32m%s\x1b[0m', message);
   }
 
-  function showFullLogs(allTestsPassing, results) {
-    allTestsPassing ? logSuccess('All tests passed !') : logError('Some tests failed !');
+  function showFullLogs(areAllTestsPassing, testResults) {
+    areAllTestsPassing ? logSuccess('All tests passed !') : logError('Some tests failed !');
 
-    results.forEach(([testResult, passing], index) => {
+    testResults.forEach(([testResult, passing], index) => {
       const { startUrl, test: { _id } } = testResult;
 
       const title = `${index + 1}.[${passing ? 'PASSED' : 'FAILED'}] ${startUrl}`
@@ -26,38 +54,10 @@ const testId = '64c608336f03cb8cdb7d955b';
       }
     });
 
-    if (!allTestsPassing) {
+    if (!areAllTestsPassing) {
       process.exit(1);
     }
   }
-
-  const landingPagesToTest = [
-    'business/de/mr-summer-ready-july-2023',
-    'business/de/winback-summer-ready-july-2023',
-    'business/en/cross-sell-2023-mobile-launch',
-    'business/en/cross-sell-flash-sale-pm-2023',
-    'business/en/independenceday',
-    'business/en/mr-independence-day-2023',
-    'business/en/mr-summer-ready-aug-2023',
-    'business/en/mr-summer-ready-july-2023',
-    'business/en/ransomwaretrial',
-    'business/en/upgrade-2023-summer-ready-aug',
-    'business/en/winback-2023-independence-day',
-    'business/en/winback-summer-ready-aug-2023',
-    'business/en/winback-summer-ready-july-2023',
-    'business/es/mr-summer-ready-july-2023',
-    'business/es/winback-summer-ready-july-2023',
-    'business/fr/mr-summer-ready-july-2023',
-    'business/fr/winback-summer-ready-july-2023',
-    'business/it/mr-summer-ready-july-2023',
-    'business/it/winback-summer-ready-july-2023',
-    'business/nl/ransomwaretrial',
-    'business/ro/mr-summer-ready-july-2023',
-    'business/ro/winback-summer-ready-july-2023',
-    'consumer/en/new/cl-offer-opt',
-    'consumer/en/new/last-offer',
-    'consumer/en/new/ultimate-flv1',
-  ];
 
   try {
     let promises = [];
@@ -70,9 +70,9 @@ const testId = '64c608336f03cb8cdb7d955b';
 
     const result = await Promise.all(promises);
 
-    const passing = result.every(([res, passing]) => passing === true);
+    const areAllTestsPassing = result.every(([res, passing]) => passing === true);
 
-    showFullLogs(passing, result);
+    showFullLogs(areAllTestsPassing, result);
   } catch (err) {
     process.exit(1);
   }
