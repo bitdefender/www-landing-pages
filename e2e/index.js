0265bff5fec956d408ff78f70fdd848bfee2158c
ext-vradulescu <ext-vradulescu@bitdefender.com>
2023-07-30 10:52:21 +0300

    DEX-14247-Ghost-inspector-integration data-testid logs

--- e2e/index.js
+++ e2e/index.js
@@ -3,16 +3,20 @@ const GhostInspector = require('ghost-inspector')(process.env.GI_KEY);
 const landingPagesTestSuiteId = '64be463282bf299ccb6b9341';
 
 (async () => {
+  const exitErr = () => {
+    console.error(`Landing pages tests suite failed, check tests output here: https://app.ghostinspector.com/suites/${landingPagesTestSuiteId}, ErrorStackTrace:${err}`);
+    process.exit(1);
+  };
+
   try {
     const [results, passing, screenshotPassing] = await GhostInspector.executeSuite(
       landingPagesTestSuiteId, { immediate: 0 }
     );
     console.log('Landing pages passing: ', passing);
     if (!passing) {
-      process.exit(1);
+      exitErr();
     }
   } catch (err) {
-    console.error(`Landing pages tests suite failed, check tests output here: https://app.ghostinspector.com/suites/${landingPagesTestSuiteId}, ErrorStackTrace:${err}`);
-    process.exit(1);
+    exitErr();
   }
 })();
