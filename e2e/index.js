462a64c67039407517925ba34e842021e77f2a34
ext-vradulescu <ext-vradulescu@bitdefender.com>
2023-07-30 12:17:11 +0300

    DEX-14247-Ghost-inspector-integration data-testid logs

--- e2e/index.js
+++ e2e/index.js
@@ -3,20 +3,41 @@ const GhostInspector = require('ghost-inspector')(process.env.GI_KEY);
 const landingPagesTestSuiteId = '64be463282bf299ccb6b9341';
 
 (async () => {
-  const exitErr = () => {
-    console.error(`Landing pages tests suite failed, check tests output here: https://app.ghostinspector.com/suites/${landingPagesTestSuiteId}, ErrorStackTrace:${err}`);
-    process.exit(1);
-  };
+  function logError(message) {
+    console.log('\x1b[31m%s\x1b[0m', message);
+  }
+  function logSuccess(message) {
+    console.log('\x1b[32m%s\x1b[0m', message);
+  }
+
+  function showFullLogs(isTestSuitePassing, results) {
+    isTestSuitePassing ? logSuccess('Test suite passed !') : logError('Test suite failed !');
+
+    results.forEach((testResult, index) => {
+      const { passing, name, _id } = testResult;
+
+      const title = `${index + 1}.[${passing ? 'PASSED' : 'FAILED'}] ${name}`
+
+      if (passing) {
+        logSuccess(title)
+      } else {
+        logError(title);
+        console.log(`Full test details on: https://app.ghostinspector.com/tests/${_id}`);
+      }
+    });
+
+    if (!isTestSuitePassing) {
+      process.exit(1);
+    }
+  }
 
   try {
-    const [results, passing, screenshotPassing] = await GhostInspector.executeSuite(
+    const [results, passing] = await GhostInspector.executeSuite(
       landingPagesTestSuiteId, { immediate: 0 }
     );
-    console.log('Landing pages passing: ', passing);
-    if (!passing) {
-      exitErr();
-    }
+
+    showFullLogs(passing, results);
   } catch (err) {
-    exitErr();
+    process.exit(1);
   }
 })();
