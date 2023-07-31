6b8f9646cf58b13c4ba4ebd570b149539b69fbdb
ext-vradulescu <ext-vradulescu@bitdefender.com>
2023-07-31 15:56:26 +0300

    DEX-14247-Ghost-inspector-integration data-testid yaml final

--- e2e/index.js
+++ e2e/index.js
@@ -2,33 +2,6 @@ require('dotenv').config();
 const GhostInspector = require('ghost-inspector')(process.env.GI_KEY);
 
 const testId = '64c608336f03cb8cdb7d955b';
-const landingPagesToTest = [
-  'business/de/mr-summer-ready-july-2023',
-  'business/de/winback-summer-ready-july-2023',
-  'business/en/cross-sell-2023-mobile-launch',
-  'business/en/cross-sell-flash-sale-pm-2023',
-  'business/en/independenceday',
-  'business/en/mr-independence-day-2023',
-  'business/en/mr-summer-ready-aug-2023',
-  'business/en/mr-summer-ready-july-2023',
-  'business/en/ransomwaretrial',
-  'business/en/upgrade-2023-summer-ready-aug',
-  'business/en/winback-2023-independence-day',
-  'business/en/winback-summer-ready-aug-2023',
-  'business/en/winback-summer-ready-july-2023',
-  'business/es/mr-summer-ready-july-2023',
-  'business/es/winback-summer-ready-july-2023',
-  'business/fr/mr-summer-ready-july-2023',
-  'business/fr/winback-summer-ready-july-2023',
-  'business/it/mr-summer-ready-july-2023',
-  'business/it/winback-summer-ready-july-2023',
-  'business/nl/ransomwaretrial',
-  'business/ro/mr-summer-ready-july-2023',
-  'business/ro/winback-summer-ready-july-2023',
-  'consumer/en/new/cl-offer-opt',
-  'consumer/en/new/last-offer',
-  'consumer/en/new/ultimate-flv1',
-];
 
 (async () => {
   function logError(message) {
@@ -42,13 +15,14 @@ const landingPagesToTest = [
     areAllTestsPassing ? logSuccess('All tests passed !') : logError('Some tests failed !');
 
     testResults.forEach(([testResult, passing], index) => {
-      const { startUrl, test: { _id } } = testResult;
+      const { variables: { name }, test: { _id } } = testResult;
 
-      const title = `${index + 1}.[${passing ? 'PASSED' : 'FAILED'}] ${startUrl}`
+      const title = `${index + 1}.[${passing ? 'PASSED' : 'FAILED'}] ${name}`
 
       if (passing) {
         logSuccess(title)
       } else {
+        // console.log('testResult', testResult);
         logError(title);
         console.log(`Full test details on: https://app.ghostinspector.com/tests/${_id}`);
       }
@@ -60,12 +34,24 @@ const landingPagesToTest = [
   }
 
   try {
+    const response = await fetch('https://main--helix-poc--enake.hlx.page/active-landingpages.json');
+
+    const activeLandingpages = await response.json();
+
     let promises = [];
 
-    landingPagesToTest.forEach(landingPagePath => {
-      promises.push(GhostInspector.executeTest(
-        testId, { startUrl: `https://${process.env.BRANCH_NAME}--helix-poc--enake.hlx.page/${landingPagePath}` }
-      ));
+    activeLandingpages.data.forEach(({URI, Products}) => {
+      const productsAsList = Products.split(',');
+
+      productsAsList.forEach((productName, index) => {
+        promises.push(GhostInspector.executeTest(
+          testId, {
+            name: `${URI} => ${productName.trim()}`,
+            productIndex: index,
+            startUrl: `https://${process.env.BRANCH_NAME}--helix-poc--enake.hlx.page/${URI}`,
+          }
+        ));
+      });
     })
 
     const result = await Promise.all(promises);
