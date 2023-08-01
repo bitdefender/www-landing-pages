91dd91f54e588e5f8b8177a469f7b148442e4b43
ext-vradulescu <ext-vradulescu@bitdefender.com>
2023-08-01 14:19:01 +0300

    DEX-14247-Ghost-inspector-integration data-testid yaml final

--- e2e/index.js
+++ e2e/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const GhostInspector = require('ghost-inspector')(process.env.GI_KEY);
 
 const testId = '64c608336f03cb8cdb7d955b';
+const snapshotsSuiteId = '64c8d884960593b38bb68331';
+const featureBranchEnvironmentBaseUrl = `https://${process.env.BRANCH_NAME}--helix-poc--enake.hlx.page`;
 
 (async () => {
   function logError(message) {
@@ -10,8 +12,7 @@ const testId = '64c608336f03cb8cdb7d955b';
   function logSuccess(message) {
     console.log('\x1b[32m%s\x1b[0m', message);
   }
-
-  function showFullLogs(testResults) {
+  function showGenericFunctionalTestsFullLogs(testResults) {
     const areAllTestsPassing = testResults.every(([res, passing]) => passing === true);
     areAllTestsPassing ? logSuccess('All tests passed !') : logError('Some tests failed !');
 
@@ -33,30 +34,62 @@ const testId = '64c608336f03cb8cdb7d955b';
     }
   }
 
+  function showSnapshotTestsFullLogs(testResults) {
+    const areAllTestsPassing = testResults.every(([res, passing, snapshotPassing]) => snapshotPassing === true);
+    areAllTestsPassing ? logSuccess('All snapshots passed !') : logError('Some snapshots failed !');
+
+    testResults.forEach(([testResult, passing, snapshotPassing], index) => {
+      const { variables: { name }, test: { _id } } = testResult;
+
+      const title = `${index + 1}.[${snapshotPassing ? 'PASSED' : 'FAILED'}] ${name}`
+
+      if (snapshotPassing) {
+        logSuccess(title)
+      } else {
+        logError(title);
+        console.log(`Full test details on: https://app.ghostinspector.com/tests/${_id}`, testResult.variables);
+      }
+    });
+
+    if (!areAllTestsPassing) {
+      process.exit(1);
+    }
+  }
+
   try {
-    const response = await fetch('https://main--helix-poc--enake.hlx.page/active-landingpages.json');
+    const [suiteTests, activeLandingpagesRes] = await Promise.all([
+      GhostInspector.getSuiteTests(snapshotsSuiteId),
+      fetch('https://main--helix-poc--enake.hlx.page/active-landingpages.json')
+    ]);
 
-    const activeLandingpages = await response.json();
+    const activeLandingpages = await activeLandingpagesRes.json();
 
-    let promises = [];
-
-    activeLandingpages.data.forEach(({URI, Products}) => {
+    const genericFunctionalTestPromises = activeLandingpages.data.flatMap(({ URI, Products }) => {
       const productsAsList = Products.split(',');
 
-      productsAsList.forEach((productName, index) => {
-        promises.push(GhostInspector.executeTest(
-          testId, {
-            name: `${URI} => ${productName.trim()}`,
-            productIndex: index,
-            startUrl: `https://${process.env.BRANCH_NAME}--helix-poc--enake.hlx.page/${URI}`,
-          }
-        ));
+      return productsAsList.map((productName, index) => {
+        return GhostInspector.executeTest(testId, {
+          name: `${URI} => ${productName.trim()}`,
+          productIndex: index,
+          startUrl: `${featureBranchEnvironmentBaseUrl}/${URI}`,
+        });
       });
-    })
+    });
 
-    const result = await Promise.all(promises);
+    const snapshotsPromises = suiteTests.map(({_id, name}) => GhostInspector.executeTest(
+      _id, {
+        name: `Snapshot => ${name}`,
+        startUrl: `${featureBranchEnvironmentBaseUrl}/${name}`,
+      }
+    ));
 
-    showFullLogs(result);
+    const [genericFunctionalTestResult, snapshotsResult] = await Promise.all([
+      Promise.all(genericFunctionalTestPromises),
+      Promise.all(snapshotsPromises)
+    ]);
+
+    showGenericFunctionalTestsFullLogs(genericFunctionalTestResult);
+    showSnapshotTestsFullLogs(snapshotsResult);
   } catch (err) {
     console.error(err);
     process.exit(1);
