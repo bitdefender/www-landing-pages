0a851a1b86a1ee38e8686212e01dfb8ad57d32cf
ext-vradulescu <ext-vradulescu@bitdefender.com>
2023-08-07 09:06:27 +0300

    DEX-14347-New-carousel

---
+++ blocks/b-big-carousel-quotes/b-big-carousel-quotes.js
@@ -0,0 +1,109 @@
+import SvgLoaderComponent from '../../components/svg-loader/svg-loader.js';
+import { generateUuidv4 } from '../../scripts/utils.js';
+import { isView } from '../../scripts/scripts.js';
+
+export default function decorate(block) {
+  const carouselSlides = [...block.children];
+  const arrowSvg = new SvgLoaderComponent('arrow-left').render();
+  const carouselId = generateUuidv4();
+  const offsetWidth = 2.5;
+  const maxSlideNr = carouselSlides.length - 1;
+  let isDesktopView = isView('desktop');
+
+  function render() {
+    block.innerHTML = `
+       <div id="${carouselId}" class="carousel slide carousel-fade" data-bs-wrap="false">
+          <div class="carousel-indicators">
+            ${carouselSlides.map((slide, idx) => `
+              <div
+                role="button"
+                data-bs-target="#${carouselId}"
+                data-bs-slide-to="${idx}"
+                class="${idx === 0 ? 'active' : ''}"
+                aria-current="${idx === 0 ? 'true' : 'false'}"
+                aria-label="Slide ${idx + 1}">
+                ${slide.children[0].children[4].innerHTML}
+              </div>
+            `).join('')}
+          </div>
+  
+          <div class="carousel-inner">
+            ${carouselSlides.map((slide, idx) => `
+              <div class="carousel-item ${idx === 0 ? 'active' : ''}">
+                <div class="inner">
+                  <q>${slide.children[0].children[0].innerText}</q>
+                  <div class="separator"></div>
+                  <p class="author">${slide.children[0].children[1].innerText}</p>
+                  <p class="position">${slide.children[0].children[2].innerText}</p>
+                  <a class="read-more" href="${slide.children[0].children[3].children[0].href}">Read More</a> 
+                </div>
+                ${isDesktopView ? slide.children[0].children[5].innerHTML : ''}
+              </div>
+              `).join('')}
+          </div>
+  
+          <button class="carousel-control-prev disabled" type="button" data-bs-target="#${carouselId}" data-bs-slide="prev" >
+            ${arrowSvg}
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#${carouselId}" data-bs-slide="next">
+            ${arrowSvg}
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+    `;
+
+    const carousel = document.getElementById(carouselId);
+    const carouselIndicators = carousel.querySelector('.carousel-indicators');
+    const carouselIndicatorsButton = carouselIndicators.children[1];
+    const ciButtonStyles = window.getComputedStyle(carouselIndicatorsButton);
+    const ciButtonMarginLeft = parseFloat(ciButtonStyles.marginLeft);
+    const prevButton = carousel.querySelector('.carousel-control-prev');
+    const nextButton = carousel.querySelector('.carousel-control-next');
+
+    function getIncrementScroll() {
+      return carouselIndicatorsButton.offsetWidth + ciButtonMarginLeft + offsetWidth;
+    }
+
+    function updateNavigationButtonsState(currentSlideIndex) {
+      if (currentSlideIndex === 0) {
+        prevButton.classList.add('disabled');
+      } else if (currentSlideIndex > 0 && currentSlideIndex < maxSlideNr) {
+        prevButton.classList.remove('disabled');
+        nextButton.classList.remove('disabled');
+      } else if (currentSlideIndex === maxSlideNr) {
+        nextButton.classList.add('disabled');
+      }
+    }
+
+    carousel.addEventListener('slide.bs.carousel', (event) => {
+      const { direction, to } = event;
+
+      if (direction === 'left') {
+        carouselIndicators.scrollLeft += getIncrementScroll();
+      } else {
+        carouselIndicators.scrollLeft -= getIncrementScroll();
+      }
+
+      updateNavigationButtonsState(to);
+    });
+
+    const ref = document.querySelector('.b-big-carousel-quotes-container');
+
+    const resizeObserver = new ResizeObserver(() => {
+      const newViewportView = isView('desktop');
+
+      const viewHasChanged = isDesktopView !== newViewportView;
+
+      if (viewHasChanged) {
+        isDesktopView = newViewportView;
+
+        render();
+      }
+    });
+
+    resizeObserver.observe(ref);
+  }
+
+  render();
+}
