e6429dc1f9b90c2dbc70b88c7cf9e13add22bd83
ltiseanu <ltiseanu@bitdefender.com>
2023-09-01 13:54:24 +0300

    update

--- blocks/b-buybuttons/b-buybuttons.js
+++ blocks/b-buybuttons/b-buybuttons.js
@@ -6,7 +6,7 @@ export default function decorate(block) {
   const parentSelector = block.closest('.section');
   const metaData = parentSelector.dataset;
   const {
-    products, buttonText3, buttonText2, buttonText
+    products, buttonText3, buttonText2, buttonText,
   } = metaData;
   const productsList = products.split(',');
   const allChildren = block.children[0].children;
@@ -20,17 +20,17 @@ export default function decorate(block) {
       if (i === 2 && buttonText2) {
         // btn 2
         const [btn2txt, btn2link] = buttonText2.split(',');
-        buylink.innerHTML += `<a class="red-buy-button await-loader prodload prodload-${onSelectorClass}" ${btn2link ? `href="${btn2link}"` : `buylink-${onSelectorClass}`} data-type="buy-btn">${btn2txt}</a>`;
+        buylink.innerHTML += `<a class="red-buy-button await-loader prodload prodload-${onSelectorClass} ${!btn2link ? 'buylink-' + onSelectorClass : ''}" ${btn2link ? `href="${btn2link}"` : ''} data-type="buy-btn">${btn2txt}</a>`;
       } else if (i === 3 && buttonText3) {
         // btn 3
         const [btn3txt, btn3link] = buttonText3.split(',');
-        buylink.innerHTML += `<a class="red-buy-button await-loader prodload prodload-${onSelectorClass}" ${btn3link ? `href="${btn3link}"` : `buylink-${onSelectorClass}`} data-type="buy-btn">${btn3txt}</a>`;
+        buylink.innerHTML += `<a class="red-buy-button await-loader prodload prodload-${onSelectorClass} ${!btn3link ? 'buylink-' + onSelectorClass : ''}" ${btn3link ? `href="${btn1link}"` : ''} data-type="buy-btn">${btn3txt}</a>`;
       } else if (buttonText) {
         // default
         const [btn1txt, btn1link] = buttonText.split(',');
-        buylink.innerHTML += `<a class="red-buy-button await-loader prodload prodload-${onSelectorClass}" ${btn1link ? `href="${btn1link}"` : `buylink-${onSelectorClass}`} data-type="buy-btn">${btn1txt}</a>`;
+        buylink.innerHTML += `<a class="red-buy-button await-loader prodload prodload-${onSelectorClass} ${!btn1link ? 'buylink-' + onSelectorClass : ''}" ${btn1link ? `href="${btn1link}"` : ''} data-type="buy-btn">${btn1txt}</a>`;
       } else {
-          buylink.innerHTML += `<a class="red-buy-button buylink-${onSelectorClass} await-loader prodload prodload-${onSelectorClass}" data-type="buy-btn">Buy Now</a>`;
+        buylink.innerHTML += `<a class="red-buy-button buylink-${onSelectorClass} await-loader prodload prodload-${onSelectorClass}" data-type="buy-btn">Buy Now</a>`;
       }
 
       allChildren[i].prepend(buylink);
