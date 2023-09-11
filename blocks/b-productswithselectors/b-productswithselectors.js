25277a9acf3d539a64a1c5938e4d4401f2358f2a
ltiseanu <ltiseanu@bitdefender.com>
2023-09-11 17:15:57 +0300

    update

--- blocks/b-productswithselectors/b-productswithselectors.js
+++ blocks/b-productswithselectors/b-productswithselectors.js
@@ -176,8 +176,7 @@ export default function decorate(block) {
         ${customPrice ? '</div>' : ''}
       `;
 
-      pricesDiv.innerHTML += `<span class="prod-newprice newprice-${customPrice ? 'custom' : onSelectorClass}">${customPrice ? customPrice : ''}</span>`;
-      
+      pricesDiv.innerHTML += `<span class="prod-newprice newprice-${customPrice || onSelectorClass}">${customPrice || ''}</span>`;
       pricesDiv.innerHTML += `<span class="prod-taxes">${taxesText}</span>`;
 
       if (idx === 1 && buttonText2) {
