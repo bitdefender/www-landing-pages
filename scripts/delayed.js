9adfaeef1fb5ee7f4b1731609d71028fba07a34b
ltiseanu <ltiseanu@bitdefender.com>
2023-09-30 16:28:20 +0300

    eslint update

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -11,31 +11,36 @@ import ZuoraNLClass from './zuora.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
-/* eslint-disable no-await-in-loop */
 async function initZuoraProductPriceLogic() {
   // window.config = ZuoraNLClass.config();
   showLoaderSpinner(false);
 
   if (productsList.length) {
-    for (const item of productsList) {
-      const prodSplit = item.split('/');
-      const prodAlias = prodSplit[0].trim();
-      const prodUsers = prodSplit[1].trim();
-      const prodYears = prodSplit[2].trim();
-      const onSelectorClass = `${prodAlias}-${prodUsers}${prodYears}`;
+    try {
+      const results = await Promise.all(
+        productsList.map(async (item) => {
+          const prodSplit = item.split('/');
+          const prodAlias = prodSplit[0].trim();
+          const prodUsers = prodSplit[1].trim();
+          const prodYears = prodSplit[2].trim();
+          const onSelectorClass = `${prodAlias}-${prodUsers}${prodYears}`;
 
-      try {
-        const zuoraResult = await ZuoraNLClass.loadProduct(item);
-        showPrices(zuoraResult);
-        showLoaderSpinner(true, onSelectorClass);
-        sendAnalyticsProducts(zuoraResult, 'nl');
-      } catch (error) {
-        console.error(error);
-      }
+          const zuoraResult = await ZuoraNLClass.loadProduct(item);
+          showPrices(zuoraResult);
+          showLoaderSpinner(true, onSelectorClass);
+          sendAnalyticsProducts(zuoraResult, 'nl');
+
+          return zuoraResult;
+        })
+      );
+
+      // results is an array of the resolved promises
+      // console.log(results);
+    } catch (error) {
+      console.error(error);
     }
   }
 }
-/* eslint-enable no-await-in-loop */
 
 if (isZuoraForNetherlandsLangMode()) {
   initZuoraProductPriceLogic();
