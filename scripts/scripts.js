5a3c9a9d73de94b622d12fe42b595a5d566745ea
Manuel ENACHE <denache@bitdefender.com>
2023-10-02 11:32:18 +0300

    adds preconnect and sets scripts to defer

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -180,11 +180,11 @@ async function loadLazy(doc) {
   if (isPageNotInDraftsFolder) {
     addScript(getInstance() === 'prod'
       ? 'https://assets.adobedtm.com/8a93f8486ba4/5492896ad67e/launch-b1f76be4d2ee.min.js'
-      : 'https://assets.adobedtm.com/8a93f8486ba4/5492896ad67e/launch-3e7065dd10db-staging.min.js', {}, 'async', () => {
+      : 'https://assets.adobedtm.com/8a93f8486ba4/5492896ad67e/launch-3e7065dd10db-staging.min.js', {}, 'defer', () => {
       document.dispatchEvent(new Event(GLOBAL_EVENTS.ADOBE_MC_LOADED));
     });
 
-    addScript('https://www.googletagmanager.com/gtm.js?id=GTM-PLJJB3', {}, 'async');
+    addScript('https://www.googletagmanager.com/gtm.js?id=GTM-PLJJB3', {}, 'defer');
   }
 
   sendAnalyticsPageLoadedEvent();
