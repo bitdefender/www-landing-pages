a0602844616f6bbc97c3da51012568074c05fb07
ext-vradulescu <ext-vradulescu@bitdefender.com>
2023-10-04 19:57:05 +0300

    fragment-support design

--- blocks/support/support.js
+++ blocks/support/support.js
@@ -1,3 +1,17 @@
 export default function decorate(block) {
-  console.log(block);
+  const columns = [...block.children[0].children].map((item, idx) => ({
+    title: item.innerHTML,
+    content: block.children[1].children[idx].innerHTML,
+  }));
+
+  block.innerHTML = `
+    <div class="wrapper">
+        ${columns.map((col) => `
+          <div class="column">
+            <div class="title">${col.title}</div>
+            <div class="content">${col.content}</div>
+          </div>
+        `).join('')}
+    </div>
+  `;
 }
