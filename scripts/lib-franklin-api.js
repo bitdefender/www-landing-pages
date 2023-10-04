9c397cd5c4d73ac81a3d80c9d1f6df9e321db8a8
Manuel ENACHE <denache@bitdefender.com>
2023-10-04 12:45:52 +0300

    Fix lint for lib-franklin-api

--- scripts/lib-franklin-api.js
+++ scripts/lib-franklin-api.js
@@ -1,55 +1,52 @@
 let dataApinDomain = 'https://main--helix-poc--enake.hlx.page';
-if (import.meta && import.meta.url ) {
-    const urlObj = new URL(import.meta.url);
-    dataApinDomain = urlObj.origin;
+if (import.meta && import.meta.url) {
+  const urlObj = new URL(import.meta.url);
+  dataApinDomain = urlObj.origin;
 }
 
 /**
- * 
  * @param {HTMLDivElement} plainHTMLContainer
  * appends the dataApiDomain to the relative links
  */
 const updateLinkSources = (plainHTMLContainer) => {
-    const allSources = plainHTMLContainer.querySelectorAll('source');
-    allSources.forEach(source => {
-        if (source.srcset.startsWith('./') || source.srcset.startsWith('/')) {
-            const srcSet = source.srcset.startsWith('.') ? source.srcset.slice(1) : source.srcset;
-            source.srcset = `${dataApinDomain}${srcSet}`;
-        }
-    });
+  const allSources = plainHTMLContainer.querySelectorAll('source');
+  allSources.forEach((source) => {
+    if (source.srcset.startsWith('./') || source.srcset.startsWith('/')) {
+      const srcSet = source.srcset.startsWith('.') ? source.srcset.slice(1) : source.srcset;
+      source.srcset = `${dataApinDomain}${srcSet}`;
+    }
+  });
 
-    const allImages = plainHTMLContainer.querySelectorAll('img');
-    allImages.forEach(image => {
-        if (image.src.startsWith('./') || image.src.startsWith('/')) {
-            const imgSrc = image.src.startsWith('.') ? image.src.slice(1) : image.src;
-            image.src = `${dataApinDomain}${imgSrc}`;
-        }
-    });
+  const allImages = plainHTMLContainer.querySelectorAll('img');
+  allImages.forEach((image) => {
+    if (image.src.startsWith('./') || image.src.startsWith('/')) {
+      const imgSrc = image.src.startsWith('.') ? image.src.slice(1) : image.src;
+      image.src = `${dataApinDomain}${imgSrc}`;
+    }
+  });
 };
 
 /**
- * @param {string} offer 
+ * @param {string} offer
  * @returns {Promise<HTMLDivElement>}
  * load the block HTML
  */
 const loadBlock = async (offer) => {
+  const plainHTMLContainer = document.createElement('div');
 
-    const plainHTMLContainer = document.createElement('div');
+  try {
+    // make a call to get all the plain HTML
+    const plainHTMLResponse = await fetch(offer);
+    const plainHTML = await plainHTMLResponse.text();
 
-    try {
-        //make a call to get all the plain HTML
-        const plainHTMLResponse = await fetch(offer);
-        const plainHTML = await plainHTMLResponse.text();
+    // fill the div node with HTML
+    plainHTMLContainer.innerHTML = plainHTML;
+    updateLinkSources(plainHTMLContainer);
+  } catch (error) {
+    console.log('Failed to fetch HTML', error);
+  }
 
-        //fill the div node with HTML
-        plainHTMLContainer.innerHTML = plainHTML;
-        updateLinkSources(plainHTMLContainer);
-
-    } catch (error) {
-        console.log("Failed to fetch HTML", error);
-    }
-
-    return plainHTMLContainer;
+  return plainHTMLContainer;
 };
 
 /**
@@ -58,62 +55,62 @@ const loadBlock = async (offer) => {
  * @param {Element} shadowDom
  */
 const loadCSS = async (href, shadowDom) => {
-    if (!shadowDom.querySelector(`head > link[href="${href}"]`)) {
-        const link = document.createElement('link');
-        link.setAttribute('rel', 'stylesheet');
-        link.setAttribute('href', href);
-        
-        shadowDom.querySelector('head').appendChild(link);
-    }
-}
+  if (!shadowDom.querySelector(`head > link[href="${href}"]`)) {
+    const link = document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('href', href);
+
+    shadowDom.querySelector('head').appendChild(link);
+  }
+};
 
 /**
- * 
- * @param {string} block 
+ *
+ * @param {string} block
  * @param {Element} shadowDom
  * Franklin decorator logic
  */
 const decorateBlock = async (block, shadowDom) => {
-    try {
-        const logicModule = await import(/* webpackIgnore: true */`${dataApinDomain}/blocks/${block}/${block}.js`);
-        logicModule.default(shadowDom.querySelector(`.${block}`));
-    } catch(error) {
-        console.log("Error caused by js import", error)
-    }
+  try {
+    const logicModule = await import(/* webpackIgnore: true */`${dataApinDomain}/blocks/${block}/${block}.js`);
+    logicModule.default(shadowDom.querySelector(`.${block}`));
+  } catch (error) {
+    console.log('Error caused by js import', error);
+  }
 };
 
 /**
- * 
+ *
  * @param {string} offer -> url to the plain html
  * @param {string} block -> the requested block (needed for css and js)
  * @param {string} containerId
  * adds the requested Franklin component in the container specified through its id
  */
 export default async function addFranklinComponentToContainer(offer, block, containerId) {
-    //create a shadow DOM in the container
-    const container = document.querySelector(`#${containerId}`);
-    
-    if (!container) {
-        console.log("Process Failed due to incorrect container ID (No container was found)");
-        return;
-    }
+  // create a shadow DOM in the container
+  const container = document.querySelector(`#${containerId}`);
 
-    const shadowDom = container.attachShadow({mode: 'open'});
+  if (!container) {
+    console.log('Process Failed due to incorrect container ID (No container was found)');
+    return;
+  }
 
-    //load the Franklin block plain HTML
-    const plainHTMLContainer = await loadBlock(offer, block);
+  const shadowDom = container.attachShadow({ mode: 'open' });
 
-    if (!plainHTMLContainer.innerHTML) {
-        return;
-    }
+  // load the Franklin block plain HTML
+  const plainHTMLContainer = await loadBlock(offer, block);
 
-    //add head section to the shadowDom and append the received HTML
-    shadowDom.appendChild(document.createElement('head'));
-    shadowDom.appendChild(plainHTMLContainer);
+  if (!plainHTMLContainer.innerHTML) {
+    return;
+  }
 
-    //load the block CSS file
-    loadCSS(`${dataApinDomain}/blocks/${block}/${block}.css`, shadowDom, plainHTMLContainer);
+  // add head section to the shadowDom and append the received HTML
+  shadowDom.appendChild(document.createElement('head'));
+  shadowDom.appendChild(plainHTMLContainer);
 
-    //run the Franklin decorator logic for this block
-    await decorateBlock(block, shadowDom);
-};
+  // load the block CSS file
+  loadCSS(`${dataApinDomain}/blocks/${block}/${block}.css`, shadowDom, plainHTMLContainer);
+
+  // run the Franklin decorator logic for this block
+  await decorateBlock(block, shadowDom);
+}
