afe479e209d5b618e0b23a8ab38cd2363952c56a
ltiseanu <ltiseanu@bitdefender.com>
2023-11-09 18:20:26 +0200

    update

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -29,7 +29,7 @@ function checkForRevolut(spanSvgs, block) {
 }
 
 /**
- * decorates the header, mainly the navv
+ * decorates the header, mainly the nav
  * @param {Element} block The header block element
  */
 export default async function decorate(block) {
