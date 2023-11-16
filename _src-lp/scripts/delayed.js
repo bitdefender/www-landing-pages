f63b9c6f54cc6291c81df85a699b603df0106d64
Manuel ENACHE <denache@bitdefender.com>
2023-11-16 14:58:24 +0200

    adds sentry monitoring tool

--- _src-lp/scripts/delayed.js
+++ _src-lp/scripts/delayed.js
@@ -1,5 +1,8 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './lib-franklin.js';
+import { addScript } from './utils.js';
+
+addScript('https://js.sentry-cdn.com/31155ca43cab4235b06e5da92992eef0.min.js');
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
