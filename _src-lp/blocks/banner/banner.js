3e2cdeba224f32eb2abee4623b8e7928fa9cff6f
ltiseanu <ltiseanu@bitdefender.com>
2023-11-29 12:29:50 +0200

    DEX-15976 - Winter campaign - Landing Page

--- _src-lp/blocks/banner/banner.js
+++ _src-lp/blocks/banner/banner.js
@@ -3,16 +3,27 @@ export default function decorate(block) {
   const blockStyle = block.style;
   const metaData = block.closest('.section').dataset;
   const {
-    textColor, backgroundColor, paddingTop, paddingBottom, marginTop, marginBottom, imageCover, corners,
+    backgroundColor, textColor, underlinedInclinedTextColor, textAlignVertical, imageAlign, paddingTop, paddingBottom, marginTop, marginBottom, imageCover, corners,
   } = metaData;
   const [richTextEl, pictureEl] = [...block.children];
 
   if (backgroundColor) parentBlockStyle.backgroundColor = backgroundColor;
   if (textColor) blockStyle.color = textColor;
+  if (underlinedInclinedTextColor) {
+    block.querySelectorAll('em u').forEach(element => {
+      element.style.color = underlinedInclinedTextColor;
+      element.style.fontStyle = 'normal';
+      element.style.textDecoration = 'none';
+    });
+  };
   if (paddingTop) blockStyle.paddingTop = `${paddingTop}rem`;
   if (paddingBottom) blockStyle.paddingBottom = `${paddingBottom}rem`;
   if (marginTop) blockStyle.marginTop = `${marginTop}rem`;
   if (marginBottom) blockStyle.marginBottom = `${marginBottom}rem`;
+
+  if (corners && corners === 'round') {
+    blockStyle.borderRadius = '20px';
+  }
 
   if (corners && corners === 'round') {
     blockStyle.borderRadius = '20px';
@@ -33,22 +44,42 @@ export default function decorate(block) {
         </div>
       </div>
     `;
+  } else if (imageCover && imageCover === 'full') {
+    parentBlockStyle.background = `url(${pictureEl.querySelector('img').getAttribute('src').split('?')[0]}) no-repeat top right / auto 100% ${backgroundColor || 'transparent'}`;
+    block.innerHTML = `
+    <div class="container-fluid">
+        <div class="row d-none d-lg-flex">
+          <div class="col-12 col-md-7">${richTextEl.innerHTML}</div>
+        </div>
+        <div class="row d-lg-none justify-content-center">
+          <div class="col-12 col-md-7 text-center">${richTextEl.innerHTML}</div>
+        </div>
+      </div>
+    `;
   } else {
     block.innerHTML = `
     <div class="container-fluid">
         <div class="row d-none d-lg-flex">
           <div class="col-5 ps-4">${richTextEl.innerHTML}</div>
-          <div class="col-7">
+          <div class="col-7 img-right">
             ${pictureEl.innerHTML}
           </div>
         </div>
         <div class="row d-lg-none justify-content-center">
           <div class="col-12 p-0 text-center">
-          ${pictureEl.innerHTML}
+            ${pictureEl.innerHTML}
           </div>
           <div class="col-12 col-md-7 text-center">${richTextEl.innerHTML}</div>
         </div>
       </div>
     `;
+  }
+
+  if (textAlignVertical) {
+    block.querySelector('.row').classList.add(`align-items-${textAlignVertical}`);
+  }
+
+  if (imageAlign) {
+    block.querySelector('.img-right').style.textAlign = imageAlign;
   }
 }
