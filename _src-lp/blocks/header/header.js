312f23ccbf80f00e88b9cd4565d3bfc1b0aabf2f
ltiseanu <ltiseanu@bitdefender.com>
2024-01-03 09:33:51 +0200

    update

--- _src-lp/blocks/header/header.js
+++ _src-lp/blocks/header/header.js
@@ -50,7 +50,7 @@ export default async function decorate(block) {
 
     block.classList.add('lp-header', 'py-3');
 
-    if (window.location.href.indexOf('scuderiaferrari') !== -1 || window.location.href.indexOf('spurs') !== -1 || window.location.href.indexOf('spurs') !== -1) {
+    if (window.location.href.indexOf('scuderiaferrari') !== -1 || window.location.href.indexOf('spurs') !== -1) {
       headerWrapper.id = 'headerFerrari';
       headerWrapper.classList.add('headerSpurs', 'dark');
       block.innerHTML = html;
