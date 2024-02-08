64b9fb40ae3e894fc7a302e45c99814a6903ea62
ext-vradulescu <ext-vradulescu@bitdefender.com>
2024-02-08 13:06:34 +0200

    region-id-france

--- ghosti/index.js
+++ ghosti/index.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const path = require('path');
 const SnapshotBlockTest = require('./json-tests/snapshot-block');
 require('dotenv').config();
 const GhostInspector = require('ghost-inspector')(process.env.GI_KEY);
@@ -8,6 +7,7 @@ const snapshotsSuiteId = '64c8d884960593b38bb68331';
 const featureBranchEnvironmentBaseUrl = `https://${process.env.BRANCH_NAME || 'main'}--www-landing-pages--bitdefender.hlx.page`;
 const pathToBlocks = 'sidekick/blocks';
 const localBlocksPath = '_src-lp/blocks';
+const FETCH_TIMEOUT = 1000 * 60 * 6; // 6 minutes
 
 (async () => {
   function logError(message) {
@@ -22,19 +22,21 @@ const localBlocksPath = '_src-lp/blocks';
   }
 
   function showSnapshotTestsFullLogs(testResults) {
-    const mappedTests = testResults.map((test) => test.data);
+    const mappedTests = testResults.map((test) => test.data).flat();
     const areAllTestsPassing = mappedTests.every(snapshotIsPassing);
     areAllTestsPassing ? logSuccess('All snapshots passed !') : logError('Some snapshots failed !');
 
     mappedTests.forEach((testResult, index) => {
+      console.log(`test result ${index}`, testResult);
       const {
         name,
         test: { _id },
+        viewportSize: { width, height }
       } = testResult;
 
       const isPassing = snapshotIsPassing(testResult);
 
-      const title = `${index + 1}.[${isPassing ? 'PASSED' : 'FAILED'}] ${name}`;
+      const title = `${index + 1}.[${isPassing ? 'PASSED' : 'FAILED'}] ${name} on [${width}x${height}]`;
 
       if (isPassing) {
         logSuccess(title);
@@ -55,12 +57,16 @@ const localBlocksPath = '_src-lp/blocks';
     // get snapshots tests
     const snapshotSuiteTests = await GhostInspector.getSuiteTests(snapshotsSuiteId);
 
-    const snapshotsPromises = blockSnapshotsToTest
+    const snapshotsPromises = blockSnapshotsToTest.filter((item, idx) => idx < 2)
       .map((testName) => {
         const testAlreadyExists = snapshotSuiteTests.find((originalTest) => originalTest.name === testName);
 
+        console.log('testAlreadyExists', testAlreadyExists);
+
         if (testAlreadyExists) {
-          return fetch(`https://api.ghostinspector.com/v1/tests/${testAlreadyExists._id}/execute/?apiKey=${process.env.GI_KEY}&startUrl=${featureBranchEnvironmentBaseUrl}/${pathToBlocks}/${testAlreadyExists.name}`).then((res) => res.json());
+          return fetch(`https://api.ghostinspector.com/v1/tests/${testAlreadyExists._id}/execute/?apiKey=${process.env.GI_KEY}&startUrl=${featureBranchEnvironmentBaseUrl}/${pathToBlocks}/${testAlreadyExists.name}`, {
+            timeout: FETCH_TIMEOUT
+          }).then((res) => res.json());
         }
 
         return GhostInspector.importTest(snapshotsSuiteId, new SnapshotBlockTest({
