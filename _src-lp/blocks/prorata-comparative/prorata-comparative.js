943a995bd6585b95aecc198121dd70148d0fc6e9
ltiseanu <ltiseanu@bitdefender.com>
2024-04-04 16:25:18 +0300

    New upadte- adding prodBox element

--- _src-lp/blocks/prorata-comparative/prorata-comparative.js
+++ _src-lp/blocks/prorata-comparative/prorata-comparative.js
@@ -1,31 +1,35 @@
-import { getDefaultLanguage } from '../../scripts/utils.js';
+import { productAliases } from '../../scripts/scripts.js';
+import { updateProductsList } from '../../scripts/utils.js';
 
 export default function decorate(block) {
   const parentSelector = block.closest('.section');
-  const urlParams = window.location.search;
+  const metaData = parentSelector.dataset;
+  const { product, style } = metaData;
+  if (product) {
+    updateProductsList(product);
+    const [prodName, prodUsers, prodYears] = product.split('/');
+    const onSelectorClass = `${productAliases(prodName)}-${prodUsers}${prodYears}`;
+    const priceBoxTable = block.querySelector('div div:nth-of-type(3) table strong');
 
-  // Remove the leading '?' character
-  const params = new URLSearchParams(urlParams.substring(1));
-  const price = params.get('PRICE');
-  const currency = params.get('CURRENCY');
-  const src = params.get('SRC');
-  const license = params.get('LICENSE');
-  const upgradeprod = params.get('UPGRADEPROD');
-  const upgradeopt = params.get('UPGRADEOPT');
-  const lang = getDefaultLanguage();
-  let prorataPrice = 0;
-  let renewPrice = 0;
-  const buybutton = `https://store.bitdefender.com/order/upgrade.php?LICENSE=${license}&UPGRADEPROD=${upgradeprod}&UPGRADEOPT=${upgradeopt}&ORDERSTYLE=nLWw45SpnHI=&LANG=${lang}&CURRENCY=${currency}&DCURRENCY=${currency}&SRC=${src}`;
+    const prodBox = document.createElement('div');
+    prodBox.classList.add('prices_box', 'await-loader', 'prodload', `prodload-${onSelectorClass}`);
+    prodBox.innerHTML += `<span class="prod-save-box">${priceBoxTable.innerText.replace(' 0', '')} <b class="prod-save save-${onSelectorClass}"></b></span>`;
+    prodBox.innerHTML += `<span class="prod-oldprice oldprice-${onSelectorClass}"></span>`;
+    prodBox.innerHTML += `<span class="prod-newprice newprice-${onSelectorClass}"></span>`;
+    // prodBox.innerHTML += `<div class="buy_box"><a class="red-buy-button buylink-${onSelectorClass} await-loader prodload prodload-${onSelectorClass}" referrerpolicy="no-referrer-when-downgrade">${block.querySelector('div div:nth-of-type(3) table a').innerText}</a></div>`;
+    priceBoxTable.innerHTML = prodBox.outerHTML;
 
-  if (currency === 'USD') {
-    prorataPrice = `${price} ${currency}`;
-    renewPrice = `${price} ${currency}`;
+    const anchorElement = block.querySelector('div div:nth-of-type(3) table a');
+    const newAnchorElement = document.createElement('a');
+    newAnchorElement.classList.add(`buylink-${onSelectorClass}`, 'await-loader', `prodload`, `prodload-${onSelectorClass}`);
+    newAnchorElement.setAttribute('referrerpolicy', 'no-referrer-when-downgrade');
+    newAnchorElement.textContent = anchorElement.textContent;
+
+    anchorElement.replaceWith(newAnchorElement);
+
   } else {
-    prorataPrice = `${currency} ${price}`;
-    renewPrice = `${currency} ${price}`;
+    block.innerHTML = 'Product metadata is mandatory';
   }
 
-  block.innerHTML = block.innerHTML.replace('0.0', prorataPrice);
-  block.innerHTML = block.innerHTML.replace('XX', renewPrice);
-  block.querySelector('table a').setAttribute('href', buybutton);
+
 }
