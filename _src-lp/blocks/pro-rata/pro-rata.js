35f43ae4cfdbe925be450c73adc096d95697ae42
ltiseanu <ltiseanu@bitdefender.com>
2024-04-09 20:19:47 +0300

    DEX-17785 - remove functions outside of decorate()

--- _src-lp/blocks/pro-rata/pro-rata.js
+++ _src-lp/blocks/pro-rata/pro-rata.js
@@ -6,6 +6,53 @@ import {
   GLOBAL_EVENTS,
 } from '../../scripts/utils.js';
 
+// get param value:
+const getParameterValue = (parameterName) => {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(parameterName);
+};
+
+// check eligibility
+const checkEligibility = async (block, optionCode) => {
+  try {
+    const serviceId = getParameterValue('service_id');
+    const url = `https://www.bitdefender.com/site/Main/proRata?service_id=${serviceId}&option_code=${optionCode}`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      console.error('Response Status:', response.status);
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+    // if eligible - redirect
+    if (data.eligible && data.url) {
+      setTimeout(() => {
+        window.location.href = data.url;
+      }, 2000);
+    } else {
+      // if not eligible - display message
+      block.innerHTML = `
+        <div class="container">
+          <div class="row">
+            <div class="text-center">
+              ${neeligibil.innerHTML}
+            </div>
+          </div>
+        </div>
+      `;
+    }
+  } catch (error) {
+    console.error('Error during fetch:', error);
+  }
+}
+
 export default function decorate(block) {
   if (window.ADOBE_MC_EVENT_LOADED) {
     sendAnalyticsPageLoadedEvent(true);
@@ -21,52 +68,6 @@ export default function decorate(block) {
   if (!optionCode) {
     block.innerHTML = 'option_code must be set';
     return;
-  }
-
-  const getParameterValue = (parameterName) => {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(parameterName);
-  };
-
-  // check eligibility
-  async function checkEligibility() {
-    try {
-      const serviceId = getParameterValue('service_id');
-      const url = `https://www.bitdefender.com/site/Main/proRata?service_id=${serviceId}&option_code=${optionCode}`;
-      const options = {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      };
-
-      const response = await fetch(url, options);
-      if (!response.ok) {
-        console.error('Response Status:', response.status);
-        throw new Error('Network response was not ok');
-      }
-
-      const data = await response.json();
-      // if eligible - redirect
-      if (data.eligible && data.url) {
-        setTimeout(() => {
-          window.location.href = data.url;
-        }, 2000);
-      } else {
-        // if not eligible - display message
-        block.innerHTML = `
-          <div class="container">
-            <div class="row">
-              <div class="text-center">
-                ${neeligibil.innerHTML}
-              </div>
-            </div>
-          </div>
-        `;
-      }
-    } catch (error) {
-      console.error('Error during fetch:', error);
-    }
   }
 
   // service_id must be set
@@ -100,6 +101,6 @@ export default function decorate(block) {
       </div>
     `;
 
-    checkEligibility();
+    checkEligibility(block, optionCode);
   }
 }
