32979818d27a4460811db15e93196089a93b2eb3
ltiseanu <ltiseanu@bitdefender.com>
2024-04-17 20:50:50 +0300

    Adding counter block

---
+++ _src-lp/blocks/counter-bf/counter-bf.js
@@ -0,0 +1,111 @@
+/* eslint-disable no-template-curly-in-string */
+export default function decorate(block) {
+  // get data attributes set in metaData
+  const parentSelector = block.closest('.section');
+  const metaData = parentSelector.dataset;
+
+
+  // config new elements
+  const {
+    textColor, backgroundColor, paddingTop, paddingBottom, marginTop,
+    marginBottom, counterSwitchOn, counterHeadings, counterTheme,
+  } = metaData;
+
+  const [contentEl, pictureBF, pictureCM] = [...block.children];
+
+  if (counterSwitchOn) {
+    // adding neccessary scripts:
+    // js
+    const flipClockJs = document.createElement('script');
+    flipClockJs.src = 'https://cdn.jsdelivr.net/npm/flipdown@0.3.2/src/flipdown.min.js';
+    document.head.appendChild(flipClockJs);
+
+    // css
+    const flipClockCss = document.createElement('link');
+    flipClockCss.rel = 'stylesheet';
+    flipClockCss.type = 'text/css';
+    flipClockCss.href = 'https://cdn.jsdelivr.net/npm/flipdown@0.3.2/dist/flipdown.min.css';
+    document.head.appendChild(flipClockCss);
+
+    // config
+    const flipClockConfig = {
+      theme: counterTheme || 'dark',
+      switchOn: new Date(counterSwitchOn).getTime() / 1000,
+      headings: counterHeadings ? counterHeadings.split(',') : '',
+    };
+
+    // additional classes for each image
+    const bannerImageBf = pictureBF.querySelector('picture');
+    bannerImageBf.classList.add('pictureBF', 'banner-image', 'flipClock-image');
+
+    const bannerImageCM = pictureCM.querySelector('picture');
+    bannerImageCM.classList.add('pictureCM', 'banner-image', 'flipClock-image');
+    bannerImageCM.style.display = 'none';
+
+    block.innerHTML = `
+      <div class="container-fluid">
+        <div class="row d-block d-lg-flex position-relative">
+          <div class="col-12 d-block d-sm-none d-md-none d-lg-none p-0 text-center bck-img">
+            ${pictureBF.innerHTML}
+            ${pictureCM.innerHTML}
+          </div>
+
+          <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 ps-4 counter-text">${contentEl.innerHTML}</div>
+
+          <div class="col-6 d-none d-sm-block d-md-block d-lg-block img-right bck-img">
+            ${pictureBF.innerHTML}
+            ${pictureCM.innerHTML}
+          </div>
+        </div>
+      </div>`;
+
+    // replacing [count]
+    block.innerHTML = block.innerHTML.replace('<p>[counter]</p>', `
+      <div style="display: none" id="flipdown" class="flipdown"></div>
+    `);
+
+    const counterSwitchOnUpdated = new Date(counterSwitchOn).getTime() / 1000;
+    const newTime = Number(counterSwitchOnUpdated) + 48 * 60 * 60;
+    const currentTime = Math.floor(Date.now() / 1000);
+    if (newTime > currentTime) {
+      block.querySelector('#flipdown').style.display = 'block';
+      setTimeout(() => {
+        // eslint-disable-next-line no-undef
+        const firstCounter = new FlipDown(Number(counterSwitchOnUpdated), flipClockConfig);
+        if (!firstCounter.countdownEnded) {
+          block.querySelectorAll('.pictureBF').forEach(elem => elem.style.display = 'block');
+          block.querySelectorAll('.pictureCM').forEach(elem => elem.style.display = 'none');
+        }
+
+        firstCounter.start()
+          .ifEnded(() => {
+            // The initial counter has ended; start a new one + 48 hours from now
+            // switch images:
+            block.querySelector('#flipdown').innerHTML = '';
+            block.querySelectorAll('.pictureBF').forEach(elem => elem.style.display = 'none');
+            block.querySelectorAll('.pictureCM').forEach(elem => elem.style.display = 'block');
+
+            const secondCounter = new FlipDown(newTime, flipClockConfig);
+            secondCounter.start();
+          });
+      }, 1000);
+    }
+
+    // update background color if set, if not set default: #000
+    if (backgroundColor) {
+      parentSelector.style.backgroundColor = backgroundColor;
+    }
+
+    if (textColor) {
+      block.style.color = textColor;
+      block.children[2].style.color = textColor;
+    }
+
+    if (paddingTop) blockStyle.paddingTop = `${paddingTop}rem`;
+    if (paddingBottom) blockStyle.paddingBottom = `${paddingBottom}rem`;
+    if (marginTop) blockStyle.marginTop = `${marginTop}rem`;
+    if (marginBottom) blockStyle.marginBottom = `${marginBottom}rem`;
+  } else {
+    block.innerHTML = 'Provide a valid counter Section Metadata';
+  }
+}
