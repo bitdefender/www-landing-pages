46bb09e1c2e35be7770077558daaf1453a646995
andserban <andserban@bitdefender.com>
2024-05-07 14:04:59 +0300

    get texts from sharepoint

--- _src-lp/blocks/exposed-form/exposed-form.js
+++ _src-lp/blocks/exposed-form/exposed-form.js
@@ -5,10 +5,16 @@ async function fetchData(url, body) {
 }
 
 export default function decorate(block) {
-  const [title, subtitle, check1, check2, estimated] = block.children;
+  const [title, subtitle, check1, check2, estimated, formButton, leaksTitle, leaksSubtitle, noLeaksTitle, noLeaksSubtitle, productTitle, productDescription, productButton, moneyBack, salesTax, tos] = block.children;
+  const elementsToHide = [...block.children].slice(5); 
+
+  elementsToHide.forEach(element => {
+    element.classList.add('hide');
+  });
   title.classList.add('title-class');
   subtitle.classList.add('subtitle-class');
 
+  console.log(leaksSubtitle.querySelector('div').innerHTML);
   //  handler function
   async function submitHandler(event) {
     event.preventDefault();
@@ -61,19 +67,19 @@ export default function decorate(block) {
         }, 1000);
       });
       //  Make the second request using the scan_id from the first request
-      const secondRequest = await fetchData(
-        'https://nimbus.bitdefender.net/lid/privacy_check',
-        {
-          id: 2,
-          jsonrpc: '2.0',
-          method: 'get_on_demand_issues',
-          params: {
-            scan_id: firstRequest.scan_id,
-          },
-        },
-      );
+      // const secondRequest = await fetchData(
+      //   'https://nimbus.bitdefender.net/lid/privacy_check',
+      //   {
+      //     id: 2,
+      //     jsonrpc: '2.0',
+      //     method: 'get_on_demand_issues',
+      //     params: {
+      //       scan_id: firstRequest.scan_id,
+      //     },
+      //   },
+      // );
 
-      // const secondRequest = { total_count: 6 };
+      const secondRequest = { total_count: 6 };
 
       block
         .querySelector('.frame-2 h4:nth-child(3)')
@@ -162,7 +168,7 @@ export default function decorate(block) {
   //  add submit button
   const submitButton = document.createElement('button');
   submitButton.setAttribute('type', 'submit');
-  submitButton.textContent = 'START MY FREE SCAN';
+  submitButton.textContent = formButton.querySelector('div').textContent;
   submitButton.setAttribute('value', 'Submit');
   // submitButton.addEventListener('click', submitHandler);
 
@@ -194,6 +200,7 @@ export default function decorate(block) {
   // create third frame container
   const thirdDiv = document.createElement('div');
   thirdDiv.classList.add('frame-3', 'hide');
-  thirdDiv.innerHTML = '<div class="hide"><h3>Result: 0 leaks so far!</h3><p>Good news: We did not detect any of your personal data on the Dark Web. However, this does not mean a database breach won’t leak your data in the future.</p></div><div class="hide"><h3>Uh oh...{numberOfLeaks} leaks detected</h3><p>Leaks in your digital footprint put you at <strong>HIGH RISK</strong> for identity theft: <strong>Hackers can exploit to take your accounts and commit fraud</strong></p></div><div class="product-wrapper"><div class="product"><div class="highlight" style="display=none"><span></span></div><p><strong>Get Bitdefender Digital Identity Protection</strong></p><p>12 months of 24/7 protection from data exposures for half price! (Price for one individual plan for the first year)</p><hr><p class="button-container"><a href="https://www.bitdefender.com.au/site/Store/buy/dip/1/1/?CART=1&amp;CARD=2&amp;SHORT_FORM=1&amp;adobe_mc=MCMID%3D55008387836489704111747435518190578875%7CMCORGID%3D0E920C0F53DA9E9B0A490D45%2540AdobeOrg%7CTS%3D1696231074" title="PROTECT MY PERSONAL DATA NOW" class="button"><span class="button-text">PROTECT MY PERSONAL DATA NOW</span></a></p><p>30-Day Money-Back Guarantee</p><p>Plus applicable sales tax.<br>See <a href="/solutions/digital-identity-protection#terms-of-use" title="Terms of Use">Terms of Use</a> below.</p></div></div><div class="error hide"><h3>Error</h3><p>Please try again after sometime.</p></div>';
+
+  thirdDiv.innerHTML = `<div class="hide"><h3>${noLeaksTitle.querySelector('div').innerHTML}</h3>${noLeaksSubtitle.querySelector('div').innerHTML}</div><div class="hide"><h3>${leaksTitle.querySelector('div').innerHTML}</h3>${leaksSubtitle.querySelector('div').innerHTML}</div><div class="product-wrapper"><div class="product"><div class="highlight" style="display=none"><span></span></div><p>${productTitle.querySelector('div').innerHTML}</p><p>${productDescription.querySelector('div').innerHTML}</p><hr>${productButton.querySelector('div').innerHTML}<p>${moneyBack.querySelector('div').innerHTML}</p><p>${salesTax.querySelector('div').innerHTML}</p><p>${tos.querySelector('div').innerHTML}</p></div></div><div class="error hide"><h3>Error</h3><p>Please try again after sometime.</p></div>`;
   block.appendChild(thirdDiv);
 }
