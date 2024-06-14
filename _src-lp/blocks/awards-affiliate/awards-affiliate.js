6b9d9569ccc3a79d6002ec89e27c8e079ab9cb64
andserban <andserban@bitdefender.com>
2024-06-14 13:27:07 +0300

    js changes

--- _src-lp/blocks/awards-affiliate/awards-affiliate.js
+++ _src-lp/blocks/awards-affiliate/awards-affiliate.js
@@ -1,17 +1,8 @@
 export default function decorate(block) {
-  const childrenArray = block.children;
-  if (childrenArray.length < 2) {
-    console.error('Not enough children to select the second-to-last element.');
-    return;
-  }
-  const iconSubtitle = childrenArray[1];
-  const subtitble = childrenArray[3];
-  const columnTitle = childrenArray[5];
-  const columnSubtitle = childrenArray[6];
-  const columns = childrenArray[childrenArray.length - 2];
+  const [, iconSubtitle, , subtitle, , columnTitle, columnSubtitle, columns] = block.children;
 
   columns.classList.add('columns-class');
-  subtitble.classList.add('subtitle');
+  subtitle.classList.add('subtitle');
   columnTitle.classList.add('columns-title');
   columnSubtitle.classList.add('columns-subtitle');
   iconSubtitle.classList.add('icon-subtitle');
