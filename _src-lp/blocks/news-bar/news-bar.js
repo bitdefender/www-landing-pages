fd701e11c8cf4f4c66b069cc6a3e3fe105a5fedb
Matei Iordache <matei.iordache21@gmail.com>
2024-06-26 11:39:52 +0300

    Add news-bar component styles and JavaScript functionality

---
+++ _src-lp/blocks/news-bar/news-bar.js
@@ -0,0 +1,9 @@
+export default function decorate(block) {
+  setTimeout(() => {
+    const element = block.querySelector('.news-bar');
+    const elementLink = block.querySelector('.news-bar a');
+    element.style.backgroundColor = '#E4F2FF';
+    element.style.color = '#006EFF';
+    if (elementLink) elementLink.style.color = '#006EFF';
+  }, 2000);
+}
