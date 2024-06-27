9258f6ac979016529c848ddeca3c77df9fbc8946
ltiseanu <ltiseanu@bitdefender.com>
2024-06-27 10:52:32 +0300

    test

--- _src-lp/blocks/banner/banner.js
+++ _src-lp/blocks/banner/banner.js
@@ -259,8 +259,8 @@ export default function decorate(block) {
 
     const imageCoverVar = imageCover.split('-')[1];
     if (imageCoverVar) {
-      console.log('sdfaasd')
       blockStyle.background = `url(${pictureEl.querySelector('img').getAttribute('src').split('?')[0]}) no-repeat top ${imageCoverVar} / auto 100% ${innerBackgroundColor || '#000'}`;
+      console.log('innerBackgroundColor ', innerBackgroundColor)
     }
 
     let defaultSize = 'col-sm-6 col-md-6 col-lg-5';
