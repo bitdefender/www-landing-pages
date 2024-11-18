f17cc580796d2680adecfb4b33373151f034865c
ltiseanu <ltiseanu@bitdefender.com>
2024-11-18 14:59:05 +0200

    updates news-bar

--- _src-lp/blocks/news-bar/news-bar.js
+++ _src-lp/blocks/news-bar/news-bar.js
@@ -1,9 +1,35 @@
 export default function decorate(block) {
-  setTimeout(() => {
-    const element = block.closest('.section');
-    const elementLink = block.querySelector('a');
-    element.style.backgroundColor = '#E4F2FF';
-    block.style.color = '#006EFF';
-    if (elementLink) elementLink.style.color = '#006EFF';
-  }, 2000);
+  const parentBlock = block.closest('.section');
+  const parentBlockStyle = parentBlock.style;
+  const blockStyle = block.style;
+  const metaData = parentBlock.dataset;
+  const [, backgroundEl] = block.children;
+  const {
+    type, backgroundColor, textColor,
+  } = metaData;
+
+  // set the back
+  if (backgroundEl) {
+    const backgroundImgEl = backgroundEl.querySelector('img');
+    const backgroundImgSrc = backgroundImgEl?.getAttribute('src');
+
+    if (backgroundImgSrc) {
+      parentBlockStyle.backgroundImage = `url("${backgroundImgSrc}")`;
+      // Remove the row after setting background
+      backgroundEl.remove();
+    }
+  }
+
+  if (type && type === 'ribbon') {
+    parentBlock.classList.add('topRibbon');
+    parentBlockStyle.backgroundColor = backgroundColor || 'black';
+    blockStyle.color = textColor || 'white';
+  } else {
+    setTimeout(() => {
+      const elementLink = block.querySelector('a');
+      parentBlockStyle.backgroundColor = '#E4F2FF';
+      blockStyle.color = '#006EFF';
+      if (elementLink) elementLink.style.color = '#006EFF';
+    }, 2000);
+  }
 }
