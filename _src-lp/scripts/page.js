85abf636027d2a0196e30efa23959964408ecbdf
iconstantin@bitdefender.com <iconstantin@bitdefender.com>
2025-04-16 17:42:17 +0300

    prepared Target webSDK integration

---
+++ _src-lp/scripts/page.js
@@ -0,0 +1,29 @@
+import { Page } from "@repobit/dex-utils";
+import { Locale } from "./vendor/product.js";
+
+/**
+ * Returns the environment based on the hostname
+ * @returns {String}
+ */
+const getEnvironment = () => {
+  const hostToInstance = {
+    'bitdefender.com': 'prod',
+    'hlx.page': 'stage',
+    'hlx.live': 'stage',
+  };
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const [host, inst] of Object.entries(hostToInstance)) {
+    if (window.location.hostname.includes(host)) return inst;
+  }
+
+  return 'dev';
+}
+
+/**
+ * @returns {string} page name
+ */
+const getPageName = () => window.location.pathname.split('/').filter(Boolean).pop();
+
+export default new Page(await Locale.get(), getPageName(), getEnvironment());
+
