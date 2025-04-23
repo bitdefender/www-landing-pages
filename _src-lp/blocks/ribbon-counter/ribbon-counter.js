0999ff7c593c21318340ce809ec1fab4b1b5b201
ltiseanu <ltiseanu@bitdefender.com>
2025-04-23 12:15:37 +0300

    DEX-22741 - French Days / Flash Sales Ribbon with Countdown for Product Pages & Landing Pages

---
+++ _src-lp/blocks/ribbon-counter/ribbon-counter.js
@@ -0,0 +1,51 @@
+export default function decorate(block) {
+  const parentSection = block.closest('.section');
+  const { backgroundColor, textColor, counterSwitchOn, counterTheme, counterHeadings, paddingTop, paddingBottom, marginTop, marginBottom } = parentSection.dataset;  
+  const [, backgroundEl] = block.children;
+
+  if (backgroundColor) parentSection.style.backgroundColor = backgroundColor;
+  if (textColor) block.querySelector("*").style.color = textColor;
+  if (paddingTop) block.style.paddingTop = `${paddingTop}rem`;
+  if (paddingBottom) block.style.paddingBottom = `${paddingBottom}rem`;
+  if (marginTop) parentSection.style.marginTop = `${marginTop}rem`;
+  if (marginBottom) parentSection.style.marginBottom = `${marginBottom}rem`;
+
+  if (backgroundEl) {
+    const backgroundImgEl = backgroundEl.querySelector('img');
+    const backgroundImgSrc = backgroundImgEl?.getAttribute('src');
+
+    if (backgroundImgSrc) {
+      block.closest('.section').style.backgroundImage = `url("${backgroundImgSrc}")`;
+      // Remove the row after setting background
+      backgroundEl.remove();
+    }
+  }
+
+  if (counterSwitchOn) {
+    block.innerHTML = block.innerHTML.replace('[counter]', `
+      <strong class="ribbonCounter"></strong>
+    `);
+    const countdownElement = block.querySelector('.ribbonCounter');
+    const targetDate = new Date(counterSwitchOn).getTime();
+
+    function updateCountdown() {
+      const now = new Date().getTime();
+      const distance = targetDate - now;
+
+      if (distance < 0) {
+        // countdownElement.innerHTML = "Countdown ended!";
+        parentSection.remove();
+        clearInterval(interval);
+        return;
+      }
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      countdownElement.innerHTML = `${days}j : ${hours}h : ${minutes}min : ${seconds}s`;
+    }
+
+    const interval = setInterval(updateCountdown, 1000);
+    updateCountdown();
+  }
+}
