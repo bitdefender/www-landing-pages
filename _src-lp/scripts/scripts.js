c16a578883b9b355b22b9b20d06a98e9b9d77d9e
iconstantin@bitdefender.com <iconstantin@bitdefender.com>
2025-05-27 12:07:13 +0300

    Added post to syntasa and updated head scripts

--- _src-lp/scripts/scripts.js
+++ _src-lp/scripts/scripts.js
@@ -240,14 +240,22 @@ async function loadEager(doc) {
     await waitForLCP(LCP_BLOCKS);
   }
 
-  if (getInstance() === 'prod') {
-    (() => {
-      const script = document.createElement('script');
-      script.src = 'https://js.sentry-cdn.com/31155ca43cab4235b06e5da92992eef0.min.js';
-      script.crossOrigin = 'anonymous';
-      script.async = true;
-      document.head.appendChild(script);
-    })();
+  if (getInstance() === 'prod' && Math.random() < 0.01) {
+    window.sentryOnLoad = () => {
+      /* eslint-disable-next-line no-undef */
+      Sentry.init({
+        tracesSampleRate: 1.0,
+        allowUrls: ['www.bitdefender.com'],
+        replaysSessionSampleRate: 0.1,
+        replaysOnErrorSampleRate: 1.0,
+      });
+    };
+
+    const script = document.createElement('script');
+    script.src = 'https://js.sentry-cdn.com/31155ca43cab4235b06e5da92992eef0.min.js';
+    script.crossOrigin = 'anonymous';
+    script.async = true;
+    document.head.appendChild(script);
   }
 }
 
@@ -326,35 +334,8 @@ export async function loadLazy(doc) {
   window.sentryOnLoad = () => {
     /* eslint-disable-next-line no-undef */
     Sentry.init({
-      dsn: 'https://31155ca43cab4235b06e5da92992eef0@o4504802466004992.ingest.us.sentry.io/4505244515958784',
-      // Adds request headers and IP for users, for more info visit:
-      // https://docs.sentry.io/platforms/javascript/configuration/options/#sendDefaultPii
-      sendDefaultPii: false,
-      // Alternatively, use `process.env.npm_package_version` for a dynamic release version
-      // if your build tool supports it.
-      // release: "my-project-name@2.3.12",
-      integrations: [
-        /* eslint-disable-next-line no-undef */
-        Sentry.browserTracingIntegration(),
-        /* eslint-disable-next-line no-undef */
-        Sentry.replayIntegration(),
-      ],
-      // Set tracesSampleRate to 1.0 to capture 100%
-      // of transactions for tracing.
-      // We recommend adjusting this value in production
-      // Learn more at
-      // https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
-      tracesSampleRate: 0.1,
-      allowUrls: [/https?:\/\/(www\.)?bitdefender\.com/],
-      // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
-      // tracePropagationTargets: [
-      //   "localhost",
-      //   /^https:\/\/yourserver\.io\/api/,
-      // ],
-      // Capture Replay for 10% of all sessions,
-      // plus for 100% of sessions with an error
-      // Learn more at
-      // https://docs.sentry.io/platforms/javascript/session-replay/configuration/#general-integration-configuration
+      tracesSampleRate: 1.0,
+      allowUrls: ['www.bitdefender.com'],
       replaysSessionSampleRate: 0.1,
       replaysOnErrorSampleRate: 1.0,
     });
