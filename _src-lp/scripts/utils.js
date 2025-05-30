1de2ba015d8e8be5977623ec34636050acb9f3ad
ltiseanu <ltiseanu@bitdefender.com>
2025-05-30 10:15:11 +0300

    redo adobe_mc has logic

--- _src-lp/scripts/utils.js
+++ _src-lp/scripts/utils.js
@@ -196,14 +196,12 @@ export function appendAdobeMcLinks(selector) {
     const wrapperSelector = document.querySelector(selector);
     const hrefSelector = '[href*=".bitdefender."]';
     wrapperSelector.querySelectorAll(hrefSelector).forEach(async (link) => {
-      const [linkHref, linkTarget] = link.href.split('#');
-      const isAdobeMcAlreadyAdded = linkHref.includes('adobe_mc');
+      const isAdobeMcAlreadyAdded = link.href.includes('adobe_mc');
       if (isAdobeMcAlreadyAdded) {
         return;
       }
 
-      let destinationURLWithVisitorIDs = await target.appendVisitorIDsTo(link.href);
-      if (linkTarget) destinationURLWithVisitorIDs = destinationURLWithVisitorIDs.replace('?', `#${linkTarget}?`);
+      const destinationURLWithVisitorIDs = await Target.appendVisitorIDsTo(link.href);
       link.href = destinationURLWithVisitorIDs.replace(/MCAID%3D.*%7CMCORGID/, 'MCAID%3D%7CMCORGID');
     });
   } catch (e) {
