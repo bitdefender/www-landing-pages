4c5717e7e2e861eccd978c6dbb80bc40e5f1e692
ltiseanu <ltiseanu@bitdefender.com>
2025-07-22 19:52:05 +0300

    update

--- _src-lp/scripts/utils.js
+++ _src-lp/scripts/utils.js
@@ -75,6 +75,55 @@ export const IANA_BY_REGION_MAP = new Map([
   [72, { locale: 'en-JP', label: 'en Japan' }],
 ]);
 
+const VALICU_PRODS = {
+  av: 'com.bitdefender.cl.av',
+  is: 'com.bitdefender.cl.is',
+  tsmd: 'com.bitdefender.cl.tsmd',
+  fp: 'com.bitdefender.fp',
+  ps: 'com.bitdefender.premiumsecurity',
+  psm: 'com.bitdefender.premiumsecurity',
+  psp: 'com.bitdefender.premiumsecurityplus',
+  pspm: 'com.bitdefender.premiumsecurityplus',
+  soho: 'com.bitdefender.soho',
+  mac: 'com.bitdefender.avformac',
+  vpn: 'com.bitdefender.vpn',
+  'vpn-monthly': 'com.bitdefender.vpn',
+  pass: 'com.bitdefender.passwordmanager',
+  passm: 'com.bitdefender.passwordmanager',
+  pass_sp: 'com.bitdefender.passwordmanager',
+  pass_spm: 'com.bitdefender.passwordmanager',
+  bms: 'com.bitdefender.bms',
+  mobile: 'com.bitdefender.bms',
+  ios: 'com.bitdefender.iosprotection',
+  mobileios: 'com.bitdefender.iosprotection',
+  dip: 'com.bitdefender.dataprivacy',
+  dipm: 'com.bitdefender.dataprivacy',
+  ts_i: "com.bitdefender.tsmd.v2",
+  ts_f: "com.bitdefender.tsmd.v2",
+  ps_i: "com.bitdefender.premiumsecurity.v2",
+  ps_f: "com.bitdefender.premiumsecurity.v2",
+  us_i: "com.bitdefender.ultimatesecurityeu.v2",
+  us_i_m: "com.bitdefender.ultimatesecurityeu.v2",
+  us_f: "com.bitdefender.ultimatesecurityeu.v2",
+  us_f_m: "com.bitdefender.ultimatesecurityeu.v2",
+  us_pi: "com.bitdefender.ultimatesecurityus.v2",
+  us_pi_m: "com.bitdefender.ultimatesecurityus.v2",
+  us_pf: "com.bitdefender.ultimatesecurityus.v2",
+  us_pf_m: "com.bitdefender.ultimatesecurityus.v2",
+  us_pie: "com.bitdefender.ultimatesecurityplusus.v2",
+  us_pie_m: "com.bitdefender.ultimatesecurityplusus.v2",
+  us_pfe: "com.bitdefender.ultimatesecurityplusus.v2",
+  us_pfe_m: "com.bitdefender.ultimatesecurityplusus.v2",
+  avpm: "com.bitdefender.cl.avplus.v2",
+  ultsec: "com.bitdefender.ultimatesecurityus",
+  secpass: "com.bitdefender.securepass",
+  secpassm: "com.bitdefender.securepass",
+  vsb: "com.bitdefender.vsb",
+  vsbm: "com.bitdefender.vsb",
+  sc: "com.bitdefender.ccp",
+  scm: "com.bitdefender.ccp",
+}
+
 /**
  * Returns the instance name based on the hostname
  * @returns {String}
@@ -414,16 +463,29 @@ export function getParamByName(name, link) {
   return results ? decodeURIComponent(results[1].replace(/\+/g, " ")) : null;
 }
 
-async function fetchCampaignName() {
+async function fetchCampaignName(productId, prodUsers, prodYears) {
   try {
+    const prodName = VALICU_PRODS[productId];
+    if (!prodName) return;
+
     const campaignParam = `/campaign/${getParam('vcampaign')}` || '';
-    const response = await fetch(`https://www.bitdefender.com/p-api/v1/products/com.bitdefender.premiumsecurity.v2/locale/en-mt${campaignParam}`);
+    const response = await fetch(`https://www.bitdefender.com/p-api/v1/products/${prodName}/locale/en-mt${campaignParam}`);
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     const data = await response.json();
 
-    if (data && data.product) return getParamByName('COUPON', data.product.options[0].buyLink);
+    if (prodYears >= 1 && prodYears <= 3) prodYears *= 12;
+
+    for (const item of data.product.options) {
+      const coupon = getParamByName('COUPON', item.buyLink);
+      if (item.slots === Number(prodUsers) && item.months === prodYears && coupon) {
+        return coupon;
+      }
+
+      // if (coupon) return coupon;
+    }
+
     return false;
   } catch (error) {
     throw new Error(`Fetch error! ${error.message}`);
@@ -462,7 +524,7 @@ export async function showPrices(storeObj, triggerVPN = false, checkboxId = '', 
     let campaign = oldParams.get('COUPON');
 
     // IF DE, GET THE COUPON FROM COM
-    if (locale === 'de') campaign = await fetchCampaignName();
+    if (locale === 'de') campaign = await fetchCampaignName(productId, prodUsers, prodYears);
 
     // get params
     const lang = locale === 'de' ? 'de' : oldParams.get('LANG');
