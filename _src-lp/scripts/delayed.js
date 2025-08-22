3fddc5ff6dddfdc39c715706f4e473c496c8d2c5
ltiseanu <ltiseanu@bitdefender.com>
2025-08-22 11:28:54 +0300

    revert

--- _src-lp/scripts/delayed.js
+++ _src-lp/scripts/delayed.js
@@ -4,10 +4,5 @@ import { fetchGeoIP } from './utils.js';
 
 fetchGeoIP();
 
-document.querySelector('#banner p.button-container a').href = 'sadfdasgsfdgsdgfsd';
-document.querySelector('#banner p.button-container').setAttribute('data-hrefp', 'asdfadgdf');
-document.querySelector('#banner p.button-container a').setAttribute('data-hrefa', 'asdfadgdf');
-alert('sadgfad')
-
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
