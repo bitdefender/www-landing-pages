e3f9eba381715b7167f04a5531f5e706d381d30b
IURadu04 <radungureanu@bitdefender.com>
2025-10-07 13:45:51 +0300

    css fix

--- _src-lp/blocks/news-bar/news-bar.js
+++ _src-lp/blocks/news-bar/news-bar.js
@@ -64,7 +64,6 @@ export default function decorate(block) {
       parentBlockStyle.backgroundColor = '#E4F2FF';
       blockStyle.color = '#006EFF';
       if (elementLink) elementLink.style.color = '#006EFF';
-      blockStyle.height = 'auto';
     }, 2000);
   }
 }
