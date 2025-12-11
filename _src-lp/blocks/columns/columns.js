25cafe475c5e0d42bc5b5e3cf1a169240c4f55c4
ltiseanu <ltiseanu@bitdefender.com>
2025-12-11 11:50:24 +0200

    updates

--- _src-lp/blocks/columns/columns.js
+++ _src-lp/blocks/columns/columns.js
@@ -8,6 +8,7 @@ export default function decorate(block) {
   const metaData = getDatasetFromSection(block);
 
   const { backgroundColor, textColor, linksOpenInNewTab, product } = metaData;
+  console.log(backgroundColor)
   if (backgroundColor) block.style.backgroundColor = backgroundColor;
   if (textColor) block.style.color = textColor;
 
