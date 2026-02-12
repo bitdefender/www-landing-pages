e1e0a88c53ec0bf078e43bfebceb685e6d7a81b7
iconstantin@bitdefender.com <iconstantin@bitdefender.com>
2026-02-12 18:12:14 +0200

    feat: added buy link transformers for new store

--- _src-lp/scripts/store.js
+++ _src-lp/scripts/store.js
@@ -13,4 +13,22 @@ export default new Store({
   campaign: async () => (await target.configMbox)?.promotion || vlaicuCampaign || campaign,
   locale: page.locale,
   provider: { name: 'vlaicu' },
+  transformers: {
+    buyLink: async (param) => {
+      const products = (await target.configMbox)?.products;
+      const { buyLink, product, option } = param;
+      if (!products) {
+        return buyLink;
+      }
+
+      const monthsToYears = option.subscription / (option.subscription === 1 ? 1 : 12);
+      const extraParameters = products[product.alias]?.[`${option.devices}-${monthsToYears}`]?.extraParameters || [];
+      const buyLinkURL = new URL(buyLink);
+      extraParameters.forEach(({ key, value }) => {
+        buyLinkURL.searchParams.set(key, value);
+      });
+
+      return buyLinkURL.href;
+    },
+  },
 });
