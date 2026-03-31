b25978dbfd2db3872d60c2e4d76c24531c2b1cd3
ltiseanu <ltiseanu@bitdefender.com>
2026-03-31 11:40:31 +0300

    fix display discount percent

--- _src-lp/blocks/b-productswithinputdevices/b-productswithinputdevices.js
+++ _src-lp/blocks/b-productswithinputdevices/b-productswithinputdevices.js
@@ -108,7 +108,7 @@ export default function decorate(block) {
 
   if (bulinaText) {
     const divBulina = document.createElement('div');
-    divBulina.className = 'prod-percent green_bck_circle biggerrr has2txt';
+    divBulina.className = 'prod-percent green_bck_circle bigger has2txt';
     divBulina.innerHTML = `${bulinaText.replace(/0/g, '<b class=\'max-discount\'></b>')}`;
     block.before(divBulina);
   }
