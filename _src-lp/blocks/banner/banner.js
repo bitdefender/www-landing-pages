8a35273985d3adbc626cf5598125f8803f585a0e
abutuza <abutuza@bitdefender.com>
2026-04-23 12:28:52 +0300

    DEX-27197 | fix: multiple buttons case

--- _src-lp/blocks/banner/banner.js
+++ _src-lp/blocks/banner/banner.js
@@ -223,7 +223,7 @@ export default function decorate(block) {
         pricesBox.innerHTML += `<div class="terms">${terms.querySelector('td').innerHTML}</div>`;
         pricesBox.innerHTML += `<div class="buy_box">
           <a class="red-buy-button" ${hardcodedLinks ? '' : `await-loader prodload prodload-${onSelectorClass} buylink-${onSelectorClass}`} 
-            href="${hardcodedLinks ? buybtn.querySelector('a')?.href : '#'}" referrerpolicy="no-referrer-when-downgrade">${buybtn ? buybtn.innerText : 'Get it now'}
+            href="${hardcodedLinks ? buybtn.querySelector('a')?.href : '#'}" referrerpolicy="no-referrer-when-downgrade">${(hardcodedLinks ? buybtn.innerText : buybtn.innerHTML) ?? 'Get it now'}
           </a>
         </div>`;
       }
