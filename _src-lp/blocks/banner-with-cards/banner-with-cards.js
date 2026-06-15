30ec90203576ab1f929303fd87953998f4497559
ltiseanu <ltiseanu@bitdefender.com>
2026-06-15 14:49:46 +0300

    update

--- _src-lp/blocks/banner-with-cards/banner-with-cards.js
+++ _src-lp/blocks/banner-with-cards/banner-with-cards.js
@@ -141,7 +141,7 @@ export default function decorate(block) {
   contentEl.innerHTML = contentEl.innerHTML.replace(/xx%/g, '<span class="max-discount"></span>');
 
   // apply newprice
-  contentRightEl.innerHTML = contentRightEl.innerHTML.replace(/0.0/g, '<span class="newprice-vpn-101"></span>');
+  contentRightEl.innerHTML = contentRightEl.innerHTML.replace(/xx/g, '<span class="newprice-vpn-101"></span>');
 
   if (backgroundHide) parentBlock.classList.add(`hide-${backgroundHide}`);
   if (backgroundColor) parentBlockStyle.backgroundColor = backgroundColor;
