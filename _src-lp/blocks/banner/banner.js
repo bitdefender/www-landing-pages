01d35d6fcffc8189e6129467cfedab8ed0e73c50
ltiseanu <ltiseanu@bitdefender.com>
2026-09-22 09:54:16 +0300

    DEX-28511 - BOFU LPs - Structure Update: Various Hero Banners (10 Lps)

--- _src-lp/blocks/banner/banner.js
+++ _src-lp/blocks/banner/banner.js
@@ -16,7 +16,7 @@ const getContentSizeClasses = (size) => {
     fourth: '', // Special case handled separately
     'they-wear-our-faces': 'col-sm-12 col-md-6 col-lg-6 col-xl-7',
   };
-  return sizeMap[size] || 'col-sm-12 col-md-5 col-lg-5';
+  return sizeMap[size] || 'col-sm-12 col-md-6 col-lg-6';
 };
 
 // Helper function to apply background styles
